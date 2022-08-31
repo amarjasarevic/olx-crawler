a96310fcf71c8ca7a29fc67f9fc5539e90e8c0f6
Amar Jasarevic <amar.jasarevic@casumo.com>
2022-08-31 18:24:42 +0200

    Adjust implementation

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express        = require('express');
 const bodyParser     = require('body-parser');
-const Crawler        = require('simplecrawler');
-const Parser         = require('node-html-parser');
+const Crawler        = require('crawler');
 const fs             = require('fs');
 
 const app            = express();
 
 
 const port = 8080;
-const url  = 'https://www.olx.ba/pretraga?vrsta=samoprodaja&kanton=9&do=200000&kategorija=23&kvadrata_min=40&broj-soba_select_jednoiposoban-1-5=Jednoiposoban+%281.5%29&broj-soba_select_dvosoban-2=Dvosoban+%282%29&broj-soba_select_trosoban-3=+Trosoban+%283%29&grad%5B0%5D=3812&grad%5B1%5D=3969&grad%5B2%5D=5896&stranica=';
+const url  = 'https://www.olx.ba/pretraga?vrsta=samoprodaja&kategorija=23&do=400000&kanton=9&grad%5B0%5D=0&kvadrata_min=40&stranica=';
 
 let total = 0;
 let ids = [];
@@ -27,64 +26,41 @@ app.use(function(req, res, next) {
 });
 
 const crawleFirstPage = () => {
-    const crawler = new Crawler(`${url}1`);
+    const crawler = new Crawler({
+        maxConnections : 10,
+        callback : function (error, res, done) {
+            if (error) {
+                console.log(error);
+            } else {
+                const $ = res.$;
 
-    crawler.interval = 10000;
-    crawler.maxConcurrency = 3;
-    crawler.maxDepth = 1;
-    crawler.decodeResponses = true;
+                const totalString = $('.brojrezultata > span').text();
+                total = parseInt(totalString.replace(/\./g, ''));
 
-    crawler.on('fetchcomplete', (queueItem, responseBody) => {
-        const root = Parser.parse(responseBody);
+                const articles = $('#rezultatipretrage .obicniArtikal');
 
-        total = root.querySelector('.brojrezultata').querySelector('span').innerHTML;
+                const perPage = articles.length - 2; // because of ads
 
-        const results = root.querySelector('#rezultatipretrage');
+                for (let i = 0; i < articles.length; i++) {
+                    if (articles[i].attribs.id && articles[i].attribs.id.indexOf('art_') !== -1) {
+                        ids.push(articles[i].attribs.id);
+                    }
+                }
 
-        const articles = results.querySelectorAll('.obicniArtikal');
+                if (pages === 0) {
+                    pages = Math.ceil(total / perPage);
 
-        const perPage = articles.length - 2; // because of ads
-
-        for (let i = 0; i < articles.length; i++) {
-            if (articles[i].id && articles[i].id.indexOf('art_') !== -1) {
-                ids.push(articles[i].id);
+                    for (let i = 2; i < pages; i++) {
+                        crawler.queue(`${url}${i}`);
+                    }
+                }
             }
-        }
 
-        pages = Math.ceil(total / perPage);
-
-        // loop through other pages
-        for (let i = 1; i < pages; i++) {
-            crawlePage(i);
+            done();
         }
     });
 
-    crawler.start();
-};
-
-const crawlePage = (page) => {
-    const crawler = new Crawler(`${url}${page + 1}`);
-
-    crawler.interval = 10000;
-    crawler.maxConcurrency = 3;
-    crawler.maxDepth = 1;
-    crawler.decodeResponses = true;
-
-    crawler.on('fetchcomplete', (queueItem, responseBody) => {
-        const root = Parser.parse(responseBody);
-
-        const results = root.querySelector('#rezultatipretrage');
-
-        const articles = results.querySelectorAll('.obicniArtikal');
-
-        for (let i = 0; i < articles.length; i++) {
-            if (articles[i].id && articles[i].id.indexOf('art_') !== -1) {
-                ids.push(articles[i].id);
-            }
-        }
-    });
-
-    crawler.start();
+    crawler.queue(`${url}1`);
 };
 
 app.get('/new-app', (req, res) => {
@@ -112,6 +88,6 @@ app.get('/new-app', (req, res) => {
             }
         );
 
-        res.send('New IDs: ' + newIds);
+        res.send('New IDs: ' + newIds.map(item => (`<p><a href="https://www.olx.ba/artikal/${item.substring(4)}/">${item}</a></p>`)));
     }, 10000);
 });
